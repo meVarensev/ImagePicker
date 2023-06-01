87725cc64ff7aee6dd908b440c3d79d695173f4c
Varensev Vitaliy <vitalik.unit@gmail.com>
2023-06-01 11:36:33 +0500

    feat: add header

---
+++ src/components/header/project-details.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Typography} from "@mui/material";
+
+interface IProjectDetailsProps {
+    title: string;
+    size: string;
+}
+const ProjectDetails: React.FC<IProjectDetailsProps> = ({size , title}) => {
+    return (
+        <div>
+            <Typography variant="h5" style={{color: '#161c80'}}>{title}</Typography>
+            <Typography variant="subtitle1" style={{color: '#c1c1c1'}}>
+                Размер: {size} <br /> мм (в развороте)
+            </Typography>
+        </div>
+    );
+};
+export {ProjectDetails};
